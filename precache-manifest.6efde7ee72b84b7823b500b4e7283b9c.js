fcdd53ddc3e8056408d87861ccdcbb94ef9d09a8
kiran <koffeewithkiran77@gmail.com>
2019-07-29 17:00:48 +0530

    Updates

---
+++ precache-manifest.6efde7ee72b84b7823b500b4e7283b9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d028678b3262dc0daa54837ebd65668",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23d4ed5a6d83ed84ae8e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "23d4ed5a6d83ed84ae8e",
+    "url": "/static/js/main.c160b891.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.6efde7ee72b84b7823b500b4e7283b9c.js"
 );
 
 self.addEventListener('message', (event) => {
