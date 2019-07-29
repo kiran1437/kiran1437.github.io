63162ea951a50443db28ac2bcb962dac233ef0f2
kiran <koffeewithkiran77@gmail.com>
2019-07-29 15:00:13 +0530

    Updates

---
+++ precache-manifest.c99301fcd1dae8cde31059cdf8376336.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02d67486c2e7c757ee1e98ba5954a98",
+    "url": "/React_youtubeVids/index.html"
+  },
+  {
+    "revision": "4d1d513927bebd6e3de0",
+    "url": "/React_youtubeVids/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/React_youtubeVids/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4d1d513927bebd6e3de0",
+    "url": "/React_youtubeVids/static/js/main.965c4dc7.chunk.js"
+  },
+  {
+    "revision": "db429cd9ebea94aaa532",
+    "url": "/React_youtubeVids/static/js/runtime~main.76323748.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_youtubeVids/static/media/logo.5d5d9eef.svg"
+  }
+]);
